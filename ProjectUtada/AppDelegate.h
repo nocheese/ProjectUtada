58f4a0e9df42b6fa5b031304a4feb9c71777de2a
Chazo <y784585371@163.com>
2015-05-13 15:20:45 +0800

    Initial Commit

---
+++ ProjectUtada/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectUtada
+//
+//  Created by Chazo on 15/5/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectUtada/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectUtada
+//
+//  Created by Chazo on 15/5/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
